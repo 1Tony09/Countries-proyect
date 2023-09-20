75921788048f3e6482d17058babea95d501ff4f7
Tony <tomas.i.ramirez09@gmail.com>
2023-09-20 03:04:50 -0300

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import {Route, BrowserRouter, Switch} from "react-router-dom";
+import Home from './Views/Home/Home';
+import Landing from './Views/Landing/Landing';
+import Form from './Views/Form/Form';
+import Details from './Views/Details/Details';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className='App'>
+      <BrowserRouter>
+              <Route path={"*"} component={Navbar}/>
+          <Switch>
+              <Route exact path={"/"} component={Landing} />
+              <Route path={"/home"} component={Home} />
+              <Route path={"/form"} component={Form} />
+              <Route path={"/details/:id"} component={Details} />
+          </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- client/src/Redux/Actions/actions.js
+++ client/src/Redux/Actions/actions.js
@@ -1,3 +1,24 @@
 import axios from "axios";
 import { GET_COUNTRIES,GET_ACTIVITIES,GET_COUNTRIES_BY_ID,GET_COUNTRIES_BY_NAME,ORDER_COUNTRIES,FILTER_COUNTRIES_BY_ACTIVITY,FILTER_COUNTRIES_BY_CONTINENT, CREATE } from "../Actions/action-types";
 
+export function postActivity(state) {
+    return async function(dispatch) {
+        try {
+            await axios.post("http://localhost:3001/countries/activities/", state);
+            alert("Activity created successfully!")
+        } catch (error) {
+            console.log(error);
+        }
+    }
+};
+
+
+export function getCountries() {
+    return async function(dispatch){
+        try {
+            const response = axios.get("http://localhost:3001/countries/");
+        } catch (error) {
+            console.log(error);
+        }
+    }
+};
